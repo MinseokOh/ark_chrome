da087a62982ee0f277a03642ace9cf396b195ca0
OhMinseok <glaxer95@gmail.com>
2017-07-22 04:00:48 +0900

    ui

---
+++ static/js/ui.js
@@ -0,0 +1,36 @@
+var menu = 'home';
+
+function showHome(complete){
+	if(checkTransition() == true)
+		return;
+
+	menu = 'home';
+	$('#home').transition({
+		animation  : 'fade',
+		duration   : '0.6s',
+		onComplete : complete
+	});
+}
+
+function showInfo(complete){
+	if(checkTransition() == true)
+		return;
+
+	menu = 'info';	
+	$('#info').transition({
+		animation  : 'fade',
+		duration   : '0.6s',
+		onComplete : complete		                       
+	});
+}
+
+function checkTransition() {
+	if($('#home').transition('is animating') || $('#info').transition('is animating'))
+		return true;
+
+	return false;
+}
+
+function showAddWallet(condition){
+	$('#addwallet').dimmer(condition);
+}
